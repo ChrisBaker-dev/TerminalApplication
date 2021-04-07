3726cc0b7362d058b1c2d89ba76543326f2bfbd0
Christopher Baker <chris@Christophers-MacBook-Pro.local>
2021-04-07 12:01:16 +1000

    added .gitignore / application startup login/sign-up response

--- src/main.rb
+++ src/main.rb
@@ -1,10 +1,26 @@
 require_relative './profile.rb'
 
+# Welcoming message to the application
+# Returns string login or sign-up depending on users choice
+def welcome_message()
+    puts "-------------------------------------------------"
+    puts "Thank you for using CB Finance Tools!"
+    puts "Would you like to login or sign-up?"
+    while true
+        answer = gets.chomp!.downcase
+        puts answer
+        if answer == "login" or answer == "sign-up"
+            puts "WE HERE"
+            return answer
+        else 
+            puts "Please enter 'login' or sign-up'"
+        end
+    end
+end
 
-# controller for terminal app
+# controller for terminal appÿ
 def main()
-    jerry = Profile.new('Jerry')
-    puts jerry.username
+    welcome_message()
 
 
 
